6929357ed4b080aeb5bd59a8711a0f9d0b64143f
Nadya Prabaningrum <nadya.prabaningrum@go-jek.com>
2022-06-09 16:04:26 +0700

    update

--- src/com/moka/grandplay/Main.java
+++ src/com/moka/grandplay/Main.java
@@ -3,6 +3,7 @@ package com.moka.grandplay;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -11,6 +12,9 @@ public class Main {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     Scanner in = new Scanner(System.in);
     Play play = new Play();
+
+    Stream<String> stream = Stream.of(
+        "Anggur", "Buah", "Cerry", "Durian");
 
   }
 }
