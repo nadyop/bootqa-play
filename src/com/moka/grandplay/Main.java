9585a327f4f5bd0f3e3c387d75f9bd95ee8e1152
Nadya Prabaningrum <nadya.prabaningrum@go-jek.com>
2022-06-09 16:09:50 +0700

    update

--- src/com/moka/grandplay/Main.java
+++ src/com/moka/grandplay/Main.java
@@ -2,8 +2,9 @@ package com.moka.grandplay;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Main {
 
@@ -13,8 +14,6 @@ public class Main {
     Scanner in = new Scanner(System.in);
     Play play = new Play();
 
-    Stream<String> stream = Stream.of(
-        "Anggur", "Buah", "Cerry", "Durian");
-
+    List<String> listBuah = Arrays.asList("Anggur", "Buah", "Cherry", "Durian");
   }
 }
